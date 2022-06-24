ec84ea30dbd288a1bf0916a4851028f9c308ea43
c-chimie <cchimie303@gmail.com>
2022-06-24 21:23:43 +0000

    initial commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//create component
 const Home = () => {
+	// use state bc tasks are constantly changing
+	const [tasks, setTasks] = useState([]);
+
+	// add state to my input
+	const [input, setInput] = useState('')
+
+	// when the form is submitted, this is what happens (a task is added)
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		const addTask = {
+			id: Math.floor(Math.random() * 1000),
+			text: input,
+			completed: false
+		}
+
+		setTasks([...tasks, addTask]) // add the user input to the state (use spread operator to copy existing array of tasks)
+
+		setInput('') // make the input box appear empty again after the user submits
+	}
+
+	// delete tasks
+	const deleteTask = (id) =>{
+		let filteredTasks = [...tasks].filter((tasks) => tasks.id !== id)
+		setTasks(filteredTasks)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h1 className="todos">todos</h1>
+			<div className="list-card">
+				<form onSubmit={handleSubmit}>
+					<input 
+					value={input}
+					onChange={event => setInput(event.target.value)}
+					className="input-box" 
+					placeholder="What needs to be done?" 
+					type="text" />
+				</form>
+
+				{/* use .map to display submitted text inputs */}
+				<div className="list-item">{tasks.map(task => (
+					<div className="todo" key={task.id}>
+						<p>{task.text}<button className="button" onClick={() => deleteTask(task.id)}>	
+&#10060;</button></p>
+					</div>
+				))}
+				<p className="counter">{tasks.length} tasks left</p>
+				</div>
+
+			</div>
 		</div>
-	);
+	)
 };
 
 export default Home;
